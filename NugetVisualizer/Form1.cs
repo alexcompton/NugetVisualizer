0e9d02870f46c8fae1f341e0df09181c94785904
alexcompton <alexcompton11@gmail.com>
2016-10-18 15:02:32 -0400

    created solution

---
+++ NugetVisualizer/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NugetVisualizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            var tree = new List<TreeNode>();
+
+            Task<List<TreeNode>> task = Task.Run(() => NugetCoreClient.AddNodes());
+            tree = await task;
+
+            foreach(var node in tree)
+            {
+                treeView1.Nodes.Add(node);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NugetVisualizer/NugetCoreClient.cs
@@ -0,0 +1,58 @@
+﻿using NuGet;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NugetVisualizer
+{
+    class NugetCoreClient
+    {
+        
+        private static ConcurrentDictionary<string, IPackage> packages = new ConcurrentDictionary<string, IPackage>();
+
+        public static async Task<List<TreeNode>> AddNodes()
+        {
+            var tree = new List<TreeNode>();
+            
+            string packageID = "Newtonsoft.Json";
+            
+            IPackageRepository repo = PackageRepositoryFactory.Default.CreateRepository("https://packages.nuget.org/api/v2");
+ 
+            List<IPackage> packages = repo.FindPackagesById(packageID).ToList();
+            
+            packages = packages.Where(item => (item.IsReleaseVersion() == false)).ToList();
+            
+            foreach (IPackage p in packages)
+            {
+                var depSet = new List<TreeNode>();
+
+                if (p.DependencySets != null && p.DependencySets.Count() > 0)
+                {
+                    foreach(var ds in p.DependencySets)
+                    {
+                        var deps = new List<TreeNode>();
+
+                        if (ds.Dependencies != null && ds.Dependencies.Count() > 0)
+                        {
+                            foreach(var dep in ds.Dependencies)
+                            {
+                                deps.Add(new TreeNode(dep.Id + " ≥ " + dep.VersionSpec.MinVersion));
+                            }
+                        }
+
+                        depSet.Add(new TreeNode(ds.TargetFramework.Identifier + " " + ds.TargetFramework.Version.ToString(), deps.ToArray()));
+                    }
+                }
+
+                tree.Add(new TreeNode(p.GetFullName(), depSet.ToArray()));
+            }
+
+            await Task.Delay(0);
+            return tree;
+        }
+    }
+}
